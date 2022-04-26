0354dba79863b635b8538eae973ffb5a357f2dd8
Madhav_Bansal <bansalm5137@gmail.com>
2022-04-26 12:35:47 +0530

    changed config object

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
     
     // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: YOUR-API-KEY-HERE
+        apiKey: 'AIzaSyBUpRatLdDyqrMECIrBvRvEbqISMiM7ygY',
+    authDomain: 'fir-web-codelab-9cda5.firebaseapp.com',
+    projectId: 'fir-web-codelab-9cda5',
+    storageBucket: 'fir-web-codelab-9cda5.appspot.com',
+    messagingSenderId: '355437734122',
+    appId: '1:355437734122:web:49afdac8953a1472a60f93',
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
